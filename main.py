0ee44ca0af06866b7643f5703f736912a689634e
asoukhteh <asoukhteh@gmail.com>
2026-06-28 22:36:41 +0330

    convert from function to OOP

--- main.py
+++ main.py
@@ -5,16 +5,22 @@ import time
 
 URL = "https://api.api-ninjas.com/v1/bitcoin"
 PARAMS ={
-   "X-Api-Key" : "6v1Q63g8dVSFO7bR3E3iYHvgGO8e2B8WF16BssHT" # Log in to https://api-ninjas.com/api/bitcoin#bitcoin-endpoint
+   "X-Api-Key" : "ENTER YOUR OWN API KEY HERE." # Log in to https://api-ninjas.com/api/bitcoin#bitcoin-endpoint
 } 
 
-engine = pyttsx3.init()
+class BTC:
+    def __init__(self):
+        self.engine = pyttsx3.init()
 
-r = requests.get(url=URL, params=PARAMS)
+    def get_btc_price(self):
+        r = requests.get(url=URL, params=PARAMS)
+        data = json.loads(r.text)
+        btc_price = int(float(data["price"]))
+        self.engine.say("BTC price is {}".format(btc_price))
+        print("BTC price is {}".format(btc_price))
+        self.engine.runAndWait()
 
-data = json.loads(r.text)
-btc_price = int(float(data["price"]))
 
-engine.say("BTC price is {}".format(btc_price))
-print("BTC price is {}".format(btc_price))
-engine.runAndWait()
+btc = BTC()
+
+btc.get_btc_price()
